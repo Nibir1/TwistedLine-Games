1bd44dbb7f4a97e003d99a7b0d1fb874170d059f
Nibir1 <nahasat.nibir@gmail.com>
2024-02-02 17:15:32 +0600

    changed SITE to site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  SITE: "https://twistedlinegames.com/",
+  site: "https://twistedlinegames.com/",
   integrations: [sitemap()],
 });
